646d461e03f90798979741e833578a1e0d80de07
ReahKy23 <Kylah.kerr23@gmail.com>
2025-02-22 12:17:33 -0500

    refresh

--- script.js
+++ script.js
@@ -49,21 +49,4 @@ const photos = [
     },
 ];
 
-//display all images initially
-function displayPhotos(){
-
-    //this gets the gallery container from the DOM (html)
-    const gallery = document.getElementById('gallery');
-    
-    //go through all the photos in the "photo" array
-    photos.forEach(photo => {
-        const img = document.createElement('img');
-        //sets the imgs source to the url 
-        img.src = photo.url;
-
-        //this adds the images to the gallery
-        gallery.appendChild(img);
-    });
-
-}
-window.onload = displayPhotos
+console.log('hello');
